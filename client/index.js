22f48bbc2886141c794ff35b6c7d4958b8229ab5
Mekazstan <mekastans@gmail.com>
2023-02-16 21:59:01 -0800

    first commit

--- client/index.js
+++ client/index.js
@@ -5,10 +5,23 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();  
+  console.log("Root", root)
 
+  // Generate a proof that the current child is on the nice list
+  const index = niceList.indexOf('Norman Block');
+  if (index < 0) {
+    console.error('You are not on the nice list!');
+    return;
+  }
+  console.log('Good to go...')
+  const proof = merkleTree.getProof(index);
+
+  // Send the gift request to the server with the proof to verify proof and send/ not send gift
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: 'Norman Block',
+    proof: proof
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -8,21 +8,24 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '143df3d1f9620cd8c06c8a35a3874f0d46a805a2c1133cf507a9b21c0c373e53';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
+  console.log(req.body)
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  // Verify the proof against the Merkle root
+  const verified = verifyProof(proof, name, MERKLE_ROOT);
+  console.log(verified)
+
+  if (verified) {
+    res.send('You got a toy robot!');
+  } else {
+    res.send('You are not on the list :(');
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
